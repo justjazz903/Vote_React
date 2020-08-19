ca3f74426dd0e81307195c6a385f34a7f12360cf
justjazz903 <justjazz903@gmail.com>
2020-08-19 08:58:04 -0500

    Updates

---
+++ precache-manifest.dae8de3209c763b2d910038ec205485c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3479898fbfc997ad6e00bbdd910b256",
+    "url": "/Vote_React/index.html"
+  },
+  {
+    "revision": "f14ac461a82977c5b6e0",
+    "url": "/Vote_React/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f320e4e9cc9fab506f0b",
+    "url": "/Vote_React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f14ac461a82977c5b6e0",
+    "url": "/Vote_React/static/js/2.a9fccfc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Vote_React/static/js/2.a9fccfc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f320e4e9cc9fab506f0b",
+    "url": "/Vote_React/static/js/main.e899595e.chunk.js"
+  },
+  {
+    "revision": "ecfad24773ddf9a414eb",
+    "url": "/Vote_React/static/js/runtime-main.4356d181.js"
+  }
+]);
